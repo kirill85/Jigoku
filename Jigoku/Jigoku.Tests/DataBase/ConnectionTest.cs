21425a9ca7b01984454531857c4d77f8dc39e213
nCdy <X@nCdy.org>
2011-10-21 10:51:03 +0400

    Init

---
+++ Jigoku/Jigoku.Tests/DataBase/ConnectionTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Data;
+using Npgsql;
+using NHibernate;
+using nCrypto;
+using NHibernate.Cfg;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace DevLair.Tests.DataBase
+{
+    [TestClass]
+    public class ConnectionTest
+    {
+        private String connection = null;
+
+        [TestMethod]
+        [TestInitialize]
+        public void Connect()
+        {
+            Encrypter encrypter = new Encrypter("64bit");
+
+            String keyword = encrypter.Decrypt("QnV0IHlvdSBkb24ndCByZWFsbHkgbWVhbiBpdA==");
+
+            // Check if crypter is changed
+            Assert.AreEqual(true, encrypter.SetCrypter("AES"));
+
+            this.connection = encrypter.Decrypt
+                ("Sf+ulELX4tNuvsQtUG2EZWPJDPhm8obnrSyEm7F5mtg/eziYSjoIZndeCHk8iZOhvAuMNJBxiiatrRHTt2LVFv/vJNoo8yCHkOO7TwUWv3+l8+Wpis4TSsEvc8zHgEPIucH/OQ2tyYlMboyoFH26dZLZ2Y+Kevfu79VfqLBcKGE=", keyword);
+        }
+
+        [TestMethod]
+        public void NhibernateFluentTest()
+        {
+            Assert.IsNotNull(this.connection);
+
+            ISessionFactory session = 
+               Fluently.Configure().Database(PostgreSQLConfiguration.Standard.ConnectionString(this.connection)).BuildSessionFactory();
+
+            Assert.IsNotNull(session);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+Ugoku no naraba ugoku no naraba subete kowasuwa subete kowasuwa
+Kanashimu naraba kanashimu naraba watashi no kokoro shiroku kawareru?
+Anata no koto mo watashi no koto mo subete no koto mo mada shiranai no
+Omoi mabuta wo aketa no naraba subete kowasu no nara kuro ni nare!!!
+
+Licensed under GPL3
+===================
+
+<br/>
+
+![source-code](https://raw.github.com/Melancholia/Jigoku/master/Dev.png)
