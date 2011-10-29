1ed3f26c7398fbc66cb6b4f3d9a30df7660a6ec8
kirill85 <kirillshkrogalev@mail.ru>
2011-10-30 01:16:00 +0400

    +++

--- Jigoku/Jigoku.ORM/Repository/UserRepository.cs
+++ Jigoku/Jigoku.ORM/Repository/UserRepository.cs
@@ -1,13 +1,17 @@
 ﻿using Jigoku.Core.Entities;
 using NHibernate;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Jigoku.ORM.Repository
 {
     public class UserRepository
     {
+        private List<User> users;
 
         public UserRepository()
         {
+            users = new List<User>();
             ConfigureRepository.Init();
         }
 
@@ -19,6 +23,19 @@ namespace Jigoku.ORM.Repository
                 nickCount = session.QueryOver<User>().Where(m => m.NickName == nickName).RowCount();
             }
             return (nickCount > 0);
+        }
+
+        private User findUserByName(string nickname)
+        {
+            foreach (var user in users)
+            {
+                if (user.NickName == nickname)
+                {
+                    return user;
+                }
+                else return null;
+            }
+            return null;
         }
 
         public void AddUser(ref string nickName, ref string password, ref string PrimaryMail, byte[] userPhoto = null)
@@ -43,10 +60,38 @@ namespace Jigoku.ORM.Repository
                         {
                             transaction.Rollback();
                         }
+                        users.Add(newUser);
                     }
                 }
             }
             else throw new DuplicateValueException();
+        }
+
+        public void ChangePassword(string userNickname, string oldPassword, string newPassword)
+        {
+            using (var session = ConfigureRepository.SessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var user = findUserByName(userNickname);
+                    var modifyUser = user;
+                    try
+                    {
+                        if (oldPassword != newPassword)
+                        {
+                            modifyUser.Password = newPassword;
+                        }
+                        session.SaveOrUpdate(modifyUser);
+                        transaction.Commit();
+                    }
+                    catch (HibernateException)
+                    {
+                        transaction.Rollback();
+                    }
+                    users.Remove(user);
+                    users.Add(modifyUser);
+                }
+            }
         }
     }
 }
