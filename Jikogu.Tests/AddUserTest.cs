a936451f71c3ef4834070a60440b8178f131dff9
kirill85 <kirillshkrogalev@mail.ru>
2011-11-04 20:14:24 +0400

    +++

---
+++ Jikogu.Tests/AddUserTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Jigoku.ORM;
+using Jigoku.ORM.Repository;
+namespace Jikogu.Tests
+{
+    [TestFixture]
+    public class AddUserTest
+    {
+        [Test]
+        public void AddStubUser()
+        {
+            UserRepository repository = new UserRepository();
+            repository.AddUser("John", "Doe", "john@doe.foo");
+            Assert.IsNotNull(repository);
+        }
+    }
+}
