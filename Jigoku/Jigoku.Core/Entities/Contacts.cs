34732be2a195e67d06a51ca7a4a5e2e0ce660afd
encyrtid <encyrtid@mail.com>
2011-12-06 16:23:20 +0400

    added tests for contact entity

--- Jigoku/Jigoku.Core/Entities/Contacts.cs
+++ Jigoku/Jigoku.Core/Entities/Contacts.cs
@@ -5,12 +5,23 @@ using System.Text;
 
 namespace Jigoku.Core.Entities
 {
+    public enum ContactType
+    {
+        Icq = 1,
+        Jid = 2,
+        Msn = 3,
+        MailTo = 4
+    }
+
     public class Contacts : AbstractEntity
     {
-        public User User { get; set; }
+        /*
         public virtual string Icq { get; set; }
         public virtual string Jid { get; set; }
         public virtual string Msn { get; set; }
         public virtual string MailTo { get; set; }
+         */
+        public virtual ContactType type { get; set; }
+        public virtual string value { get; set; }
     }
 }
---
+++ Jikogu.Tests/ContactsTest.cs
@@ -0,0 +1,148 @@
+﻿using NUnit.Framework;
+using Jigoku.Core.Entities;
+using Jigoku.ORM.Repository;
+using NHibernate;
+using System;
+
+namespace Jigoku.Tests
+{
+    [TestFixture]
+    public class ContactsTests
+    {
+        ContactsRepository repository;
+        [SetUp]
+        public void Init()
+        {
+            repository = new ContactsRepository();
+        }
+
+        [Test]
+        public void AddContact()
+        {
+            var contact = new Contacts();
+            contact.type = ContactType.Icq;
+            contact.value = "111111111";
+
+            try
+            {
+                TestHelper.log("Adding contact in base");
+                repository.Add(contact);
+                using (ISession session = ConfigureRepository.SessionFactory.OpenSession())
+                {
+                    var fromDB = session.Get<Contacts>(contact.Uid);
+                    Assert.IsNotNull(fromDB);
+                    Assert.AreNotSame(contact, fromDB);
+                    Assert.AreEqual(contact.type, fromDB.type);
+                    Assert.AreEqual(contact.value, fromDB.value);
+                    TestHelper.done();
+                }
+            }
+            catch (Exception e)
+            {
+                TestHelper.error(e.Message);
+            }
+        }
+
+        [Test]
+        public void UpdateContact()
+        {
+            var contact = new Contacts();
+            contact.type = ContactType.Jid;
+            contact.value = "test1@jabber.ru";
+
+            try
+            {
+                TestHelper.log("Update contact");
+                using (ISession session = ConfigureRepository.SessionFactory.OpenSession())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(contact);
+                        transaction.Commit();
+                    }
+                }
+
+                contact.value = "test2@jabber.ru";
+                repository.Update(contact);
+
+                using (ISession session = ConfigureRepository.SessionFactory.OpenSession())
+                {
+                    var fromDB = session.Get<Contacts>(contact.Uid);
+                    Assert.IsNotNull(fromDB);
+                    Assert.AreEqual(contact.value, fromDB.value);
+                    TestHelper.done();
+                }
+            }
+            catch (Exception e)
+            {
+                TestHelper.error(e.Message);
+            }
+        }
+
+        [Test]
+        public void RemoveContact()
+        {
+            var contact = new Contacts();
+            contact.type = ContactType.MailTo;
+            contact.value = "test@mail.com";
+
+            try
+            {
+                TestHelper.log("Removing contact");
+                using (ISession session = ConfigureRepository.SessionFactory.OpenSession())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(contact);
+                        transaction.Commit();
+                    }
+                }
+
+                repository.Remove(contact);
+
+                using (ISession session = ConfigureRepository.SessionFactory.OpenSession())
+                {
+                    var fromDB = session.Get<Contacts>(contact.Uid);
+                    Assert.IsNull(fromDB);
+                    TestHelper.done();
+                }
+            }
+            catch (Exception e)
+            {
+                TestHelper.error(e.Message);
+            }
+        }
+
+        [Test]
+        public void GetByIdContact()
+        {
+            var contact = new Contacts();
+            contact.type = ContactType.Icq;
+            contact.value = "222222";
+
+            try
+            {
+                TestHelper.log("Search contact by Uid");
+                using (ISession session = ConfigureRepository.SessionFactory.OpenSession())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(contact);
+                        transaction.Commit();
+                    }
+                }
+
+                var fromDB = repository.GetById(contact.Uid);
+                Assert.IsNotNull(fromDB);
+                Assert.AreNotSame(contact, fromDB);
+                Assert.AreEqual(contact.type, fromDB.type);
+                Assert.AreEqual(contact.value, fromDB.value);
+                TestHelper.done();
+            }
+            catch (Exception e)
+            {
+                TestHelper.error(e.Message);
+            }
+        }
+    }
+}
