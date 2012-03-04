430cf50a85cf6956f08200a4a2943cc4e3be427a
kirill85 <kirillshkrogalev@mail.ru>
2012-03-04 07:08:42 +0400

    Added pair GET/POST methods Register()

--- Jigoku/Jigoku/Controllers/AccountController.cs
+++ Jigoku/Jigoku/Controllers/AccountController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Jigoku.Core.Entities;
+using Jigoku.Models;
+using Jigoku.ORM.Repository;
 
-namespace Jikogu.Controllers
+namespace Jigoku.Controllers
 {
     public class AccountController : Controller
     {
@@ -22,12 +23,25 @@ namespace Jikogu.Controllers
 		{
 			if (ModelState.IsValid)
 			{
-				return View("LogOn", person);
+				PersonRepository personRepository = new PersonRepository();//получить доступ к репозиторию ОRM
+				Jigoku.Core.Entities.Person dbPerson = new Jigoku.Core.Entities.Person{NickName = person.NickName, Password = person.Password, PrimaryMail = person.PrimaryMail, Projects = null, UserPhoto = null}; //создать объект "Пользователь"
+				personRepository.Add(dbPerson); //добавить его в БД
+				return View("UserInfo", person); //вернуть страницу с информацией о пользователе
 			}
 			else
 			{
-				return View();
+				return View("Register");
 			}
+		}
+
+		private void sendMailPerson(Person person)
+		{
+			var mailMessage = new System.Net.Mail.MailMessage();
+			var mailAddressTo = new System.Net.Mail.MailAddress(person.PrimaryMail);
+			mailMessage.Subject = "No reply";
+			mailMessage.Body = "Здравствуйте, дорогой (-ая) " + person.NickName + ", вы получили это письмо потому что зарегистрировались на портале Jigoku." + " Если вы не регистрировались у нас, игнорируйте это письмо.";
+			mailMessage.Sender = new System.Net.Mail.MailAddress("admin@jigoku.org"); //indian code? hmmm...
+			new System.Net.Mail.SmtpClient().Send(mailMessage);
 		}
     }
 }
